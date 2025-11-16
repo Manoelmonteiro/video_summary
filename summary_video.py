ccac349aa717e96835302fa16f135bf5fedf5b65
Manoel <63215619+Manoelmonteiro@users.noreply.github.com>
2025-11-16 16:45:38 -0300

    refactor: limpando funcoes que nao estavam mais sendo usadas

--- summary_video.py
+++ summary_video.py
@@ -34,7 +34,6 @@ def transcreve_Audio(api_client):
 
 
 
-
 def explica_Audio(api_client):
 
     chat_completion = api_client.chat.completions.create(
@@ -52,23 +51,6 @@ def explica_Audio(api_client):
 
 
 
-def carrega_Api():
-
-    load_dotenv(find_dotenv())
-
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-
-
-    response = client.chat.completions.create(
-        model="whisper-large-v3-turbo",
-        messages=[
-            {"role":"user","content":"Escreva uma breve historia de um programador tentando aprender python"}
-        ],
-        max_tokens=100
-    )
-
-    print(response.choices[0].text.strip())
 
 
 def baixa_Video():
@@ -93,13 +75,6 @@ def procura_mp4():
     return input_file
 
 
-def gerencia_Video():
-    baixa_Video()
-    input = ffmpeg.input(procura_mp4())
-    audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
-    video = input.video.hflip()
-    out = ffmpeg.output(audio, video,'out.mp4')
-    out.run()
 
 
 def baixa_transcreve():
